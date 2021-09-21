4c311adebaa9b6747b75ca467ab947e5a9ca32ad
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2021-09-20 23:46:36 -0700

    Modified main.cf by removing azure provider

--- Start With Terraform Basics/main.tf
+++ Start With Terraform Basics/main.tf
@@ -1,15 +1,7 @@
 ## Provider's Example
 
-# Configure the AWS Provider
+# Configure the AWS provider
 provider "aws" {
-    version = "3.53.0"
-    access_key = "AKIAXC3FBB2OJJC3DUIP"
-    secret_key = "SECRET_KEY"
-    region     = "us-east-2"
-}
-
-# Configure the Microsoft Azure Provider
-provider "azurerm" {
-    version = "2.72.0"
-    features {}
+    region = "us-west-2"
+    version = "3.59.0" 
 }
