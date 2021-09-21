196d78d69d79148f2b2058110131a946c664324a
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2021-09-21 16:37:46 -0700

    Creating centralized access key

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = "AKIAXMQMEQ35UU7KVE4K"
-  secret_key = "SECRET KEY_HERE"
-  region = "us-west-2" 
-}
-
 resource "aws_instance" "MyFirstInstance" {
   ami = "ami-0c2d06d50ce30b442"
   instance_type = "t2.micro"
--- Start With Terraform Basics/provider.tf
+++ Start With Terraform Basics/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  access_key = "AKIASMSIZOF4QD2NTPB6"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-2"
+  access_key = "AKIAXMQMEQ35UU7KVE4K"
+  secret_key = "SECRET KEY_HERE"
+  region = "us-west-2" 
 }
