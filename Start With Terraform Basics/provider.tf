f28129010c633ff3eb9f724a7c49e78bb2a275d7
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2021-09-21 17:13:58 -0700

    Removed secret key

--- Start With Terraform Basics/provider.tf
+++ Start With Terraform Basics/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   access_key = "AKIAXMQMEQ35UU7KVE4K"
-  secret_key = "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"
+  secret_key = "SECRET_ACCESS_KEY_HERE"
   region = "us-west-2" 
 }
