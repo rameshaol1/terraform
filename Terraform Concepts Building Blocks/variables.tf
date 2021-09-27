e688f7d0390864d0689e8f1e34e1b9adcc2bd660
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2021-09-26 21:45:36 -0700

    commented  aws secret key  variable

---
+++ Terraform Concepts Building Blocks/variables.tf
@@ -0,0 +1,27 @@
+#variable "AWS_SECRET_KEY" {}
+
+variable "AWS_REGION" {
+  default = "us-west-2"
+}
+
+variable "AMIS" {
+  type = map
+  default = {
+      us-east-1 = "ami-0f40c8f97004632f9"
+      us-east-2 = "ami-05692172625678b4e"
+      us-west-2 = "ami-02c8896b265d8c480"
+      us-west-1 = "ami-0cdd3aca00188622e"
+  }
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "levelup_key"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "levelup_key.pub"
+}
+
+variable "INSTANCE_USENAME" {
+  default = "rkasula"
+}
