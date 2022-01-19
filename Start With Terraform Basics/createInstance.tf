1b3fae1229305c6dc83bbe26c4933615a0074054
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-01-18 19:14:42 -0700

    modifed createInstance file

--- Start With Terraform Basics/createInstance.tf
+++ Start With Terraform Basics/createInstance.tf
@@ -1,4 +1,9 @@
-resource "aws_instance" "aws-first-instance" {
+resource "aws_instance" "aws-practice-instance" {
+   count = 3
    ami = "ami-0c2d06d50ce30b442"
    instance_type = "t2.micro"
 }
+
+   tags = {
+      Name = "aws-linux00-$(count.index)"
+   }
