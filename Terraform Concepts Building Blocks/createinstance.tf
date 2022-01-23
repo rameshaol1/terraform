d62fbd99daf4cdf0a039d2e1123a8fd2ed9c2865
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-01-22 19:46:34 -0700

    modified the contents in createinstance

--- Terraform Concepts Building Blocks/createinstance.tf
+++ Terraform Concepts Building Blocks/createinstance.tf
@@ -5,11 +5,12 @@ resource "aws_key_pair" "levelup_key" {
 
 resource "aws_instance" "MyFirstInstance" {
   ami = lookup(var.AMIS, var.AWS_REGION)
+  instance_type = "t2.micro"
   key_name = aws_key_pair.levelup_key.key_name
 }
 
 tags = {
-  name = "custom_instance"
+  name = "demoinstance"
 }
 
 provisioner "file" {
@@ -20,14 +21,14 @@ provisioner "file" {
 provisioner "remote_exec" {
   inline = [
     "chmod +x /tmp/installNginx.sh",
-    "sudo sed -i -e '/s\r$//' /tmp/installNginx.sh",  #Remove the spurious CR characters
-    "sudo /tmp/installNginx.sh",
+      "sudo sed -i -e 's/\r$//' /tmp/installNginx.sh",  # Remove the spurious CR characters.
+      "sudo /tmp/installNginx.sh",
   ]
 }
 
- connection {
-   host = coalesce(self.public_ip, self.private_ip)
-   type = "ssh"
-   user = "var.INSTANCE_USERNAME"
-   private_key = file(var.PATH_TO_PRIVATE_KEY)
- }
+connection {
+    host        = coalesce(self.public_ip, self.private_ip)
+    type        = "ssh"
+    user        = var.INSTANCE_USERNAME
+    private_key = file(var.PATH_TO_PRIVATE_KEY)
+  }
