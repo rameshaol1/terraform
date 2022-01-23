4f0e01d4bef41371b8c2909303d182a2881842cd
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-01-22 19:48:27 -0700

    modified contents in createInstance

--- Terraform Concepts Building Blocks/createinstance.tf
+++ Terraform Concepts Building Blocks/createinstance.tf
@@ -1,34 +1,35 @@
+
 resource "aws_key_pair" "levelup_key" {
-  key_name = "levelup_key"
-  public_key = file(var.PATH_TO_PUBLIC_KEY)
+    key_name = "levelup_key"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
 
-resource "aws_instance" "MyFirstInstance" {
-  ami = lookup(var.AMIS, var.AWS_REGION)
+resource "aws_instance" "MyFirstInstnace10" {
+  ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  key_name = aws_key_pair.levelup_key.key_name
-}
+  key_name      = aws_key_pair.levelup_key.key_name
 
-tags = {
-  name = "demoinstance"
-}
+  tags = {
+    Name = "custom_instance"
+  }
 
-provisioner "file" {
-  source = "installNginx.sh"
-  destination = "/tmp/installNginx.sh"
-}
+  provisioner "file" {
+      source = "installNginx.sh"
+      destination = "/tmp/installNginx.sh"
+  }
 
-provisioner "remote_exec" {
-  inline = [
-    "chmod +x /tmp/installNginx.sh",
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/installNginx.sh",
       "sudo sed -i -e 's/\r$//' /tmp/installNginx.sh",  # Remove the spurious CR characters.
       "sudo /tmp/installNginx.sh",
-  ]
-}
+    ]
+  }
 
-connection {
+  connection {
     host        = coalesce(self.public_ip, self.private_ip)
     type        = "ssh"
     user        = var.INSTANCE_USERNAME
     private_key = file(var.PATH_TO_PRIVATE_KEY)
   }
+}
