7bcf08f8734878b8f895d940aad7a409b38339c9
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-01-24 18:40:10 -0700

    Created Security.tf file

---
+++ Terraform Concepts Building Blocks/Demo DataSource/securitygroup.tf
@@ -0,0 +1,21 @@
+data "aws_ip_ranges" "us_west_ip_range" {
+    regions = ["us-west-1,us-west-2"]
+    services = ["ec2"]
+}
+
+resource "aws_security_group" "sg-custom_us_west" {
+    name = "sg-custom_us_west"
+
+    ingress  {
+        from_port = "443"
+        to_port = "443"
+        protocol = "tcp"
+        cidr_blocks = "data.aws_ip_ranges.us_west_ip_range.cidr_blocks"
+    }
+
+    tags = {
+      CreateDate = "data.aws_ip_ranges.us_west_ip_range.create_date"
+      SyncToken = "data.aws_ip_ranges.us_west_ip_range.sync_token"
+
+    }
+}
