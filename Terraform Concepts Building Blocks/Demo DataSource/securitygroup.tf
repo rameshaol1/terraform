27d3a752e5fac3d5682c1646b8830420b386557a
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-01-24 18:49:51 -0700

    Modified the file

--- Terraform Concepts Building Blocks/Demo DataSource/securitygroup.tf
+++ Terraform Concepts Building Blocks/Demo DataSource/securitygroup.tf
@@ -1,20 +1,20 @@
-data "aws_ip_ranges" "us_east_ip_range" {
-    regions = ["us-east-1","us-east-2"]
+data "aws_ip_ranges" "us_west_ip_range" {
+    regions = ["us-west-1","us-west-2"]
     services = ["ec2"]
 }
 
-resource "aws_security_group" "sg-custom_us_east" {
-    name = "custom_us_east"
+resource "aws_security_group" "sg-custom_us_west" {
+    name = "custom_us_west"
 
     ingress {
         from_port = "443"
         to_port = "443"
         protocol = "tcp"
-        cidr_blocks = slice(data.aws_ip_ranges.us_east_ip_range.cidr_blocks, 0, 50)
+        cidr_blocks = slice(data.aws_ip_ranges.us_west_ip_range.cidr_blocks, 0, 50)
     }
 
     tags = {
-        CreateDate = data.aws_ip_ranges.us_east_ip_range.create_date
-        SyncToken = data.aws_ip_ranges.us_east_ip_range.sync_token
+        CreateDate = data.aws_ip_ranges.us_west_ip_range.create_date
+        SyncToken = data.aws_ip_ranges.us_west_ip_range.sync_token
     }
 }
