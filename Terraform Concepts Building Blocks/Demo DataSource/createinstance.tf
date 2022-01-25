837d6394e5913a13e446e047e373b68797292bec
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-01-24 17:13:04 -0700

    Modified the createInstance file

--- Terraform Concepts Building Blocks/Demo DataSource/createinstance.tf
+++ Terraform Concepts Building Blocks/Demo DataSource/createinstance.tf
@@ -1,7 +1,10 @@
+data "aws_availability_zones" "available" {}
+
 
 resource "aws_instance" "MyFirstInstnace10" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
+  availability_zone = data.aws_availability_zones.available.names[1]
   
   tags = {
     Name = "terraform-linux001"
