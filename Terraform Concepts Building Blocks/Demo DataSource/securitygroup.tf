56c585d3d6d63310fcba6fe6d8d5102104e8b438
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-01-24 18:48:14 -0700

    Modified the file

--- Terraform Concepts Building Blocks/Demo DataSource/securitygroup.tf
+++ Terraform Concepts Building Blocks/Demo DataSource/securitygroup.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "sg-custom_us_east" {
         from_port = "443"
         to_port = "443"
         protocol = "tcp"
-        cidr_blocks = "data.aws_ip_ranges.us_east_ip_range.cidr_blocks"
+        cidr_blocks = slice(data.aws_ip_ranges.us_east_ip_range.cidr_blocks, 0, 50)
     }
 
     tags = {
