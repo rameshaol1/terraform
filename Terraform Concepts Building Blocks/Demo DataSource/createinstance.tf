86d583cc6cea4aaa2e87c996b4879264ecd9a205
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-01-24 17:38:13 -0700

    Modified the file

--- Terraform Concepts Building Blocks/Demo DataSource/createinstance.tf
+++ Terraform Concepts Building Blocks/Demo DataSource/createinstance.tf
@@ -1,8 +1,21 @@
 data "aws_availability_zones" "available" {}
 
+data "aws_ami" "latest-ubuntu" {
+  most_recent = true
+  owner = ["099720109477"]
+
+  filter{
+    name = "name"
+    values = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.4-amd-server-*"
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
 resource "aws_instance" "MyFirstInstnace10" {
-  ami           = lookup(var.AMIS, var.AWS_REGION)
+  ami           = data.aws_ami.latest-ubuntu.id
   instance_type = "t2.micro"
   availability_zone = data.aws_availability_zones.available.names[1]
   
