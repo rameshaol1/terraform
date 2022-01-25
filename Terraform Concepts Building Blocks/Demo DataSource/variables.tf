36d9785a9ac15965ccfe62bdffbc231f0b8e82df
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-01-24 18:27:43 -0700

    Removed us-west AMIs

--- Terraform Concepts Building Blocks/Demo DataSource/variables.tf
+++ Terraform Concepts Building Blocks/Demo DataSource/variables.tf
@@ -18,7 +18,5 @@ variable "AMIS"{
     default = {
         us-east-1 = "ami-087c17d1fe0178315"
         us-east-2 = "ami-00dfe2c7ce89a450b"
-        us-west-2 = "ami-0892d3c7ee96c0bf7"
-        us-west-1 = "ami-011996ff98de391d1"
   }
 }
