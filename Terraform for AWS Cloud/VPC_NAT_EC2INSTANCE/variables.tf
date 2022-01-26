5dee4a6563b4f537f98baa18d5d358a17b971f65
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-01-25 18:08:13 -0700

    modified the files

--- Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/variables.tf
+++ Terraform for AWS Cloud/VPC_NAT_EC2INSTANCE/variables.tf
@@ -22,3 +22,15 @@ variable "AMIS"{
       us-west-1 = "ami-011996ff98de391d1"
   }
 }
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "levelup_key"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "levelup_key.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}
