8be6a3f84c9f2c263ab1cceb39979c2979051b6b
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-01-27 16:42:30 -0700

    modified variables file

--- Terraform for AWS Cloud/EBS Volume/variables.tf
+++ Terraform for AWS Cloud/EBS Volume/variables.tf
@@ -12,10 +12,10 @@ default = "us-west-2"
 variable "AMIS" {
     type = map
     default = {
-        us-east-1 = "ami-0f40c8f97004632f9"
-        us-east-2 = "ami-05692172625678b4e"
-        us-west-2 = "ami-0352d5a37fb4f603f"
-        us-west-1 = "ami-0f40c8f97004632f9"
+        us-east-1 = "ami-087c17d1fe0178315"
+        us-east-2 = "ami-00dfe2c7ce89a450b"
+        us-west-2 = "ami-0892d3c7ee96c0bf7"
+        us-west-1 = "ami-011996ff98de391d1"
     }
 }
 
