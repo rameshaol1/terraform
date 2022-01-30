e17a8a19273a3c5f9a4b8b0f8cc6374d35a602c6
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-01-29 23:34:35 -0700

    Modified the files

--- Terraform for AWS Cloud II/AWS RDS/createInstance.tf
+++ Terraform for AWS Cloud II/AWS RDS/createInstance.tf
@@ -6,7 +6,10 @@ resource "aws_key_pair" "levelup_key" {
 resource "aws_instance" "MyFirstInstnace" {
   ami           = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
+  availability_zone = "us-west-2b"
   key_name      = aws_key_pair.levelup_key.key_name
+  vpc_security_group_ids = [aws_security_group.allow-levelup-ssh.id]
+  subnet_id = aws_subnet.levelupvpc-public-2.id
 
   tags = {
     Name = "custom_instance"
