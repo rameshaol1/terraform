9e9f74124f44c3fc5455f59f59b690495443e465
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-01-29 23:34:56 -0700

    modified files

--- Terraform for AWS Cloud II/AWS RDS/security_group.tf
+++ Terraform for AWS Cloud II/AWS RDS/security_group.tf
@@ -1,3 +1,4 @@
+#Security Group for allowing SSH connection
 resource "aws_security_group" "allow-levelup-ssh" {
   vpc_id      = aws_vpc.levelupvpc.id
   name        = "allow-levelup-ssh"
@@ -19,5 +20,30 @@ resource "aws_security_group" "allow-levelup-ssh" {
   
   tags = {
     Name = "allow-levelup-ssh"
+  }
+}
+
+#Security group for allowing MariaDB
+resource "aws_security_group" "allow-mariadb" {
+  vpc_id      = aws_vpc.levelupvpc.id
+  name        = "allow-mariadb"
+  description = "security group that allows MariaDB"
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = [aws_security_group.allow-levelup-ssh.id]
+  }
+  
+  tags = {
+    Name = "allow-mariadb"
   }
 }
