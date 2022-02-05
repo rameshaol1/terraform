0742a3549f8bb7365d9c71975c4d6a38a011ec22
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-02-04 21:55:20 -0700

    Modified variables and iamuser tf file

--- Terraform for AWS Cloud II/AWS IAM/iamuser.tf
+++ Terraform for AWS Cloud II/AWS IAM/iamuser.tf
@@ -1,5 +1,4 @@
-#TF File for IAM Users and Groups
-
+#TF files for IAM Users and Groups
 resource "aws_iam_user" "adminuser1" {
   name = "adminuser1"
 }
@@ -8,24 +7,24 @@ resource "aws_iam_user" "adminuser2" {
   name = "adminuser2"
 }
 
-# Group TF Definition
+#Group TF Definition
 resource "aws_iam_group" "admingroup" {
-  name = "admingroup"
+  name  = "admingroup"
 }
 
-#Assign User to AWS Group
+#Assign users to AWS group
 resource "aws_iam_group_membership" "admin-users" {
   name = "admin-users"
   users = [
     aws_iam_user.adminuser1.name,
-    aws_iam_user.adminuser2.name,
+    aws_iam_user.adminuser2.name
   ]
-  group = aws_iam_group.admingroup.name
+  group = aws_iam_group.admingroup.name 
 }
 
-#Policy for AWS Group
+#Policy for AWS group
 resource "aws_iam_policy_attachment" "admin-users-attach" {
-  name       = "admin-users-attach"
-  groups     = [aws_iam_group.admingroup.name]
+  name = "admin-users-attach"
+  groups = [aws_iam_group.admingroup.name]
   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
