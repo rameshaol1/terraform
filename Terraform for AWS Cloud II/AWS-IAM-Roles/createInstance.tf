42dcd69d2b49476ebbfe8c0f46be5a8986d11185
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-02-06 22:47:12 -0700

    Modified the files in IAM ROLES

--- Terraform for AWS Cloud II/AWS-IAM-Roles/createInstance.tf
+++ Terraform for AWS Cloud II/AWS-IAM-Roles/createInstance.tf
@@ -1,2 +1,23 @@
+resource "aws_key_pair" "levelup_key" {
+    key_name = "levelup_key"
+    public_key = file(var.PATH_TO_PUBLIC_KEY) 
+}
 
+#Create AWS EC2 instance
+resource "aws_instance" "MyFirstInstance" {
+    ami = lookup(var.AMIS, var.AWS_REGION)
+    instance_type = "t2.micro"
+    availability_zone = "us-west-2b"
+    key_name = aws_key_pair.levelup_key.key_name
 
+    iam_instance_profile  = aws_iam_instance_profile.s3-levelupbucket-role-instanceprofile.name
+
+    tags = {
+        Name = "custom_instance"
+    }
+}
+
+output "public_ip" {
+    value = aws_instance.MyFirstInstance.public_ip
+  
+}
--- Terraform for AWS Cloud II/AWS-IAM-Roles/s3bucket.tf
+++ Terraform for AWS Cloud II/AWS-IAM-Roles/s3bucket.tf
@@ -1,2 +1,10 @@
+#Create AWS S3 Bucket
+resource "aws_s3_bucket" "levelup-s3bucket" {
+    bucket = "levelup-bucket-0402"
+    acl = "private"
 
+    tags = {
+      "Name" = "levelup-bucket-0402"
+    }
+}
 
