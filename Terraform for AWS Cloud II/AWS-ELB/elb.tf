ccaf559cefe45b4a23645f4adb33fb01b23828b2
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-02-07 22:16:13 -0700

    Modified the contents

--- Terraform for AWS Cloud II/AWS-ELB/elb.tf
+++ Terraform for AWS Cloud II/AWS-ELB/elb.tf
@@ -1,30 +1,30 @@
 #AWS ELB Configuration
-resource "aws_elb" "leveup_elb" {
-  name = "aws_elb"
-  subnets = [aws_subnet.levelupvpc-public-1.id, aws_subnet.levelupvpc-public-2.id]
-  security_groups = [aws_security_group.levelup-elb-aws_security_group.id]
-
+resource "aws_elb" "levelup-elb" {
+  name            = "levelup-elb"
+  subnets         = [aws_subnet.levelupvpc-public-1.id, aws_subnet.levelupvpc-public-2.id]
+  security_groups = [aws_security_group.levelup-elb-securitygroup.id]
+  
   listener {
-    instance_port = 80
+    instance_port     = 80
     instance_protocol = "http"
-    lb_port = 80
-    lb_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
   }
 
   health_check {
-    healthy_threshold = 2
+    healthy_threshold   = 2
     unhealthy_threshold = 2
-    timeout = 3
-    target = "HTTP:80/"
-    interval = 30
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
   }
 
-  cross_zone_load_balancing = true
-  connection_draining = true
+  cross_zone_load_balancing   = true
+  connection_draining         = true
   connection_draining_timeout = 400
 
   tags = {
-    "Name" = "levelup-elb"
+    Name = "levelup-elb"
   }
 }
 
@@ -84,4 +84,3 @@ resource "aws_security_group" "levelup-instance" {
     Name = "levelup-instance"
   }
 }
-
