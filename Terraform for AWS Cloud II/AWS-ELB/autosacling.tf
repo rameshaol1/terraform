c9a7846bb01113369f83c5f5dcbfb9c75e97c9fc
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-02-07 21:21:52 -0700

    Modified the contents

--- Terraform for AWS Cloud II/AWS-ELB/autosacling.tf
+++ Terraform for AWS Cloud II/AWS-ELB/autosacling.tf
@@ -1,42 +1,85 @@
-#AutoScaling Launch Configuration
+#Autoscaling Launch Configuration
 resource "aws_launch_configuration" "levelup-launchconfig" {
-  name_prefix     = "levelup-launchconfig"
-  image_id        = lookup(var.AMIS, var.AWS_REGION)
-  instance_type   = "t2.micro"
-  key_name        = aws_key_pair.levelup_key.key_name
-  security_groups = [aws_security_group.levelup-instance.id]
-  user_data       = "#!/bin/bash\napt-get update\napt-get -y install net-tools nginx\nMYIP=`ifconfig | grep -E '(inet 10)|(addr:10)' | awk '{ print $2 }' | cut -d ':' -f2`\necho 'Hello Team\nThis is my IP: '$MYIP > /var/www/html/index.html"
-
-  lifecycle {
-    create_before_destroy = true
-  }
+    name_prefix = "levelup-launchconfig"
+    image_id = lookup(var.AMIS, var.AWS_REGION)
+    instance_type = "t2.micro"
+    key_name = aws_key_pair.levelup_key.key_name
 }
 
-#Generate Key
+#Generate the key
 resource "aws_key_pair" "levelup_key" {
-    key_name = "levelup_key"
-    public_key = file(var.PATH_TO_PUBLIC_KEY)
+    key_name = "levelup_policy"
+    public_key = file(var.PATH_TO_PUBLIC_KEY)  
 }
 
 #Autoscaling Group
 resource "aws_autoscaling_group" "levelup-autoscaling" {
-  name                      = "levelup-autoscaling"
-  vpc_zone_identifier       = [aws_subnet.levelupvpc-public-1.id, aws_subnet.levelupvpc-public-2.id]
-  launch_configuration      = aws_launch_configuration.levelup-launchconfig.name
-  min_size                  = 2
-  max_size                  = 2
-  health_check_grace_period = 200
-  health_check_type         = "ELB"
-  load_balancers            = [aws_elb.levelup-elb.name]
-  force_delete              = true
+    name = "levelup-autoscaling"
+    vpc_zone_identifier = ["subnet-070460943cccd0547", "subnet-08d7b7ceb4b547f79"]
+    launch_configuration = aws_launch_configuration.levelup-launchconfig.name
+    min_size = 1
+    max_size = 2
+    health_check_grace_period = 200
+    health_check_type = "EC2"
+    force_delete = "true"
 
-  tag {
-    key                 = "Name"
-    value               = "LevelUp Custom EC2 instance via LB"
-    propagate_at_launch = true
-  }
+    tag {
+      key   = "Name"
+      value = "Levelup Custom EC2 instance"
+      propagate_at_launch = true
+    }  
 }
 
-output "ELB" {
-  value = aws_elb.levelup-elb.dns_name
+#Autoscaling Configuration Policy  - Scaling Alarm
+resource "aws_autoscaling_policy" "levelup-cpu-policy" {  
+    name = "levelup-cpu-policy"
+    autoscaling_group_name = aws_autoscaling_group.levelup-autoscaling.name
+    adjustment_type = "ChangeInCapacity"
+    scaling_adjustment = "1"
+    cooldown = "200"
+    policy_type = "SimpleScaling"
+}
+
+#Autoscaling Cloudwatch Monitoring
+resource "aws_cloudwatch_metric_alarm" "levelup-cpu-alarm" {
+    alarm_name = "levelup-cpu-alarm"
+    alarm_description = "Alarm once CPU use is increase"
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    evaluation_periods = "2"
+    metric_name = "CPUUtilization"
+    namespace = "AWS/EC2"
+    period = "120"
+    statistic = "Average"
+    threshold = "30"
+
+    dimensions = {
+        "AutoScalingGroupName" = aws_autoscaling_group.levelup-autoscaling.name
+    }
+}
+
+#Autoscaling Descaling 
+resource "aws_autoscaling_policy" "levelup-cpu-policy-scaledown" {  
+    name = "levelup-cpu-policy-scaledown"
+    autoscaling_group_name = aws_autoscaling_group.levelup-autoscaling.name
+    adjustment_type = "ChangeInCapacity"
+    scaling_adjustment = "-1"
+    cooldown = "200"
+    policy_type = "SimpleScaling"
+}
+
+#Auto Descaling Cloud Watch
+resource "aws_cloudwatch_metric_alarm" "levelup-cpu-alarm-scaledown" {
+    alarm_name = "levelup-cpu-alarm-scaledown"
+    alarm_description = "Alarm once CPU use is decrease"
+    comparison_operator = "LessThanOrEqualToThreshold"
+    evaluation_periods = "2"
+    metric_name = "CPUUtilization"
+    namespace = "AWS/EC2"
+    period = "120"
+    statistic = "Average"
+    threshold = "10"
+
+    dimensions = {
+        "AutoScalingGroupName" = aws_autoscaling_group.levelup-autoscaling.name
+    }
 }
--- Terraform for AWS Cloud II/AWS-ELB/vpc.tf
+++ Terraform for AWS Cloud II/AWS-ELB/vpc.tf
@@ -1,117 +1,115 @@
 #Create AWS VPC
 resource "aws_vpc" "levelupvpc" {
-  cidr_block       = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/16"
   instance_tenancy = "default"
-  enable_dns_support   = "true"
+  enable_dns_support = "true"
   enable_dns_hostnames = "true"
-  enable_classiclink   = "false"
 
   tags = {
-    Name = "levelupvpc"
-  }
+    "name" = "levelupvpc"
+  } 
 }
 
-# Public Subnets in Custom VPC
+#Create Public  Subnets in custom VPC
 resource "aws_subnet" "levelupvpc-public-1" {
   vpc_id                  = aws_vpc.levelupvpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-2a"
+  availability_zone = "us-west-2a"
 
   tags = {
-    Name = "levelupvpc-public-1"
+    "name" = "levelupvpc-public-1"
   }
 }
 
 resource "aws_subnet" "levelupvpc-public-2" {
   vpc_id                  = aws_vpc.levelupvpc.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-2b"
+  availability_zone = "us-west-2b"
 
   tags = {
-    Name = "levelupvpc-public-2"
+    "name" = "levelupvpc-public-2"
   }
 }
 
 resource "aws_subnet" "levelupvpc-public-3" {
   vpc_id                  = aws_vpc.levelupvpc.id
-  cidr_block              = "10.0.3.0/24"
+  cidr_block = "10.0.3.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "us-east-2c"
+  availability_zone = "us-west-2c"
 
   tags = {
-    Name = "levelupvpc-public-3"
+    "name" = "levelupvpc-public-3"
   }
 }
 
-# Private Subnets in Custom VPC
+#Create Private subnets for custom VPC
+
 resource "aws_subnet" "levelupvpc-private-1" {
   vpc_id                  = aws_vpc.levelupvpc.id
-  cidr_block              = "10.0.4.0/24"
+  cidr_block = "10.0.4.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "us-east-2a"
+  availability_zone = "us-west-2a"
 
   tags = {
-    Name = "levelupvpc-private-1"
+    "name" = "levelupvpc-private-1"
   }
 }
 
 resource "aws_subnet" "levelupvpc-private-2" {
   vpc_id                  = aws_vpc.levelupvpc.id
-  cidr_block              = "10.0.5.0/24"
+  cidr_block = "10.0.5.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "us-east-2b"
+  availability_zone = "us-west-2b"
 
   tags = {
-    Name = "levelupvpc-private-2"
+    "name" = "levelupvpc-private-2"
   }
 }
 
 resource "aws_subnet" "levelupvpc-private-3" {
   vpc_id                  = aws_vpc.levelupvpc.id
-  cidr_block              = "10.0.6.0/24"
+  cidr_block = "10.0.6.0/24"
   map_public_ip_on_launch = "false"
-  availability_zone       = "us-east-2c"
+  availability_zone = "us-west-2c"
 
   tags = {
-    Name = "levelupvpc-private-3"
+    "name" = "levelupvpc-private-3"
   }
 }
 
-# Custom internet Gateway
+#Custom Internet gateway
+
 resource "aws_internet_gateway" "levelup-gw" {
   vpc_id = aws_vpc.levelupvpc.id
 
   tags = {
-    Name = "levelup-gw"
+    "name" = "levelup-gw"
   }
 }
 
-#Routing Table for the Custom VPC
+#Route table for the custom VPC
+
 resource "aws_route_table" "levelup-public" {
   vpc_id = aws_vpc.levelupvpc.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.levelup-gw.id
   }
-
-  tags = {
-    Name = "levelup-public-1"
-  }
 }
 
 resource "aws_route_table_association" "levelup-public-1-a" {
-  subnet_id      = aws_subnet.levelupvpc-public-1.id
+  subnet_id = aws_subnet.levelupvpc-public-1.id
   route_table_id = aws_route_table.levelup-public.id
 }
 
 resource "aws_route_table_association" "levelup-public-2-a" {
-  subnet_id      = aws_subnet.levelupvpc-public-2.id
+  subnet_id = aws_subnet.levelupvpc-public-2.id
   route_table_id = aws_route_table.levelup-public.id
 }
 
 resource "aws_route_table_association" "levelup-public-3-a" {
-  subnet_id      = aws_subnet.levelupvpc-public-3.id
+  subnet_id = aws_subnet.levelupvpc-public-3.id
   route_table_id = aws_route_table.levelup-public.id
 }
