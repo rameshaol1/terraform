eb1e22b396c3214e0b5c4c48a65d470024cfd44e
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-02-09 16:28:54 -0700

    Modified the contenst

--- NetworkModule/main.tf
+++ NetworkModule/main.tf
@@ -18,14 +18,13 @@ resource "aws_key_pair" "levelup_key" {
 
 #EC2 Instance
 resource "aws_instance" "levelup_instance" {
-  ami = var.instance_ami
-  instance_type = var.instance_type
-  subnet_id = module.myvpc.public_subnet_id
-  vpc_security_group_ids = var.myvpc.sg_22_id
-  key_name = var.aws_key_pair.levelup_key.key_name
+  ami                       = var.instance_ami
+  instance_type             = var.instance_type
+  subnet_id                 = module.myvpc.public_subnet_id
+  vpc_security_group_ids    = module.myvpc.sg_22_id
+  key_name                  = aws_key_pair.levelup_key.key_name
 
   tags = {
-    Environment = var.environment_tag
-  }
-  
+		Environment         = var.environment_tag
+	}
 }
