ecf15165ac19c14e7bcd04616c3d755793117411
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-02-08 19:18:22 -0700

    Created main/provider tfin Terraform modules

---
+++ Terraform Modules/main.tf
@@ -0,0 +1,14 @@
+module "ec2-cluster" {
+    source = "https://github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
+
+    name = "my-cluster"
+    ami  = "ami-0892d3c7ee96c0bf7"
+    instance_type = "t2.micro"
+    subnet_id = "subnet-070460943cccd0547"
+
+    tags = {
+        Terraform = "true"
+        Environment = "dev"
+    }
+  
+}
---
+++ Terraform Modules/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    access_key = "AKIAUORRFWQM23JZ6TMY"
+    secret_key = ""
+    region = "us-west-2"
+}
