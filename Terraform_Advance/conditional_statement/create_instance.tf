939d35aef33499c554ec2bad7132137c06d0415b
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-02-10 14:07:50 -0700

    Modified the createinstance file

--- Terraform_Advance/conditional_statement/create_instance.tf
+++ Terraform_Advance/conditional_statement/create_instance.tf
@@ -1,19 +1,19 @@
 provider "aws" {
-  region = var.AWS_REGION  
+  region     = var.AWS_REGION
 }
 
 module "ec2_cluster" {
-  source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
-  
-  name = "my-cluster"
-  ami  = "ami-0892d3c7ee96c0bf7"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-070460943cccd0547"
-  instance_count = var.envinronment == "Production" ? 2 : 1
+    source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
 
-  tags = {
-    Terraform = "true"
-    Environment = var.envinronment
-    
-  }
+    name            = "my-cluster"
+    ami             = "ami-0892d3c7ee96c0bf7"
+    instance_type   = "t2.micro"
+    subnet_id       = "subnet-070460943cccd0547"
+    instance_count  = var.environment == "Production" ? 2 : 1
+
+
+    tags = {
+    Terraform       = "true"
+    Environment     = var.environment
+    }
 }
