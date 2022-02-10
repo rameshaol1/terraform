7bba21eedeaf090ff74b4ca4d2afdffb3af5e08b
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-02-10 14:03:05 -0700

    files created for condition

---
+++ Terraform_Advance/conditional_statement/create_instance.tf
@@ -0,0 +1,19 @@
+provider "aws" {
+  region = var.AWS_REGION  
+}
+
+module "ec2_cluster" {
+  source = "github.com/terraform-aws-modules/terraform-aws-ec2-instance.git"
+  
+  name = "my-cluster"
+  ami  = "ami-0892d3c7ee96c0bf7"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-070460943cccd0547"
+  instance_count = var.envinronment == "Production" ? 2 : 1
+
+  tags = {
+    Terraform = "true"
+    Environment = var.envinronment
+    
+  }
+}
