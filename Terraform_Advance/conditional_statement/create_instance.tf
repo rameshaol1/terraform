0d19e79558344b03bc89962f751205598ff587f1
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-02-10 14:22:42 -0700

    Modified the contents

--- Terraform_Advance/conditional_statement/create_instance.tf
+++ Terraform_Advance/conditional_statement/create_instance.tf
@@ -9,7 +9,7 @@ module "ec2_cluster" {
     ami             = "ami-0892d3c7ee96c0bf7"
     instance_type   = "t2.micro"
     subnet_id       = "subnet-070460943cccd0547"
-    instance_count  = var.environment == "Production" ? 2 : 1
+    count           = var.environment == "Production" ? 2 : 1
     
 
 
