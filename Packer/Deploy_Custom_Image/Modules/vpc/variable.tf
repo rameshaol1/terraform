ead5fb30ca088f1b9e637c7e59200620c964c585
Ramesh Kasula <ramesh.kumar.kasula@gmail.com>
2022-02-12 20:00:42 -0700

    Deploy Custom AMI   files

---
+++ Packer/Deploy_Custom_Image/Modules/vpc/variable.tf
@@ -0,0 +1,11 @@
+#Define Variable for Custom Module VPC
+
+variable "AWS_REGION" {
+    type    = string
+    default = "us-west-2"
+}
+
+variable "ENVIRONMENT" {
+    type    = string
+    default = ""
+}
